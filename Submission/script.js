4be6617f58e875b5a066e8a8445df5026b6b3410
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-03-11 11:26:58 +1300

    write first automated test for user's name

--- Submission/script.js
+++ Submission/script.js
@@ -5,13 +5,21 @@
  *
  */
 
-const myLevel = 1;
-const path = 0;
+let rememberedName = false;
+let userName = "";
+let myLevel = 1;
+let path;
 
 const getBotReply = (msg) => {
-    if (myLevel === 1 && path === 0) {
-        const userName = msg;
-        return `Hi ${userName}! Do you feel like something alcoholic?`;
+    if (rememberedName === false && myLevel === 1) {
+        rememberedName = true;
+        userName = msg;
+        myLevel = 2;
+        return `Hola ${userName}, sit back and relaaaax! Would you like an alcoholololololic tipple? Oh sorry amigo, I've already had a few myself today.`;
+    }
+
+    if (rememberedName && msg === "yes" && myLevel === 2) {
+        return `Oh you are ready to party ${userName}! You are my type of person. Do you like the feeling of bubbles in your mouth?`;
     }
 
     return "Error unknown...";
--- test/script.test.js
+++ test/script.test.js
@@ -4,18 +4,31 @@
 let getBotReply;
 
 beforeEach(() => {
-  return import("../Submission/script").then((module) => {
-    getBotReply = module.getBotReply;
-    jest.resetModules();
-  });
+    return import("../Submission/script").then((module) => {
+        getBotReply = module.getBotReply;
+        jest.resetModules();
+    });
 });
 
 describe("getBotReply", () => {
-  it("should reply with [PUT YOUR DESCRIPTION HERE]", () => {
-    const botReply1 = getBotReply("Harry");
-    const expectedReply1 = "";
+    it("should reply with greeting the user by their name", () => {
+        // * Start: "Hey thirsty lips, I'm your drink genie, your wish is my command. What's your name?"
+        // * Input: Bob
+        const botReply1 = getBotReply("Bob");
+        // * Output: "Hola Bob, sit back and relaaaax! Would you like an alcoholololololic tipple? Oh sorry amigo, I've already had a few myself today."
+        const expectedReply1 =
+            "Hola Bob, sit back and relaaaax! Would you like an alcoholololololic tipple? Oh sorry amigo, I've already had a few myself today.";
 
-    // Uncomment the following line and update your expectation
-    // expect(botReply1).toEqual(expectedReply1);
-  });
+        // * Input: "yes"
+        // * Output: "Oh you are ready to party Bob! You are my type of person. Do you like the feeling of bubbles in your mouth?"
+        // * Input: "yes"
+        // * Output: "You want a party in your mouth huh! Are you feeling sweet or bitter today?"
+        // * Input: "bitter"
+        // * Output: "I agree, bitter is better. Let me pour you a beer. Dark or light?"
+        // * Input: "dark"
+        // * Output: "Time for a Guinness.  This is a full meal in a glass! Be careful it will go straight to your head. Enjoy your drink and if you want another one, just type restart."
+
+        // Uncomment the following line and update your expectation
+        expect(botReply1).toEqual(expectedReply1);
+    });
 });
